e9385323687f55a1d17e5e7c931f8794744b3fdf
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-24 01:06:41 +0700

    docker compose commands

---
+++ 4. docker-compose.sh
@@ -0,0 +1,8 @@
+# Create containers if not exist and start
+docker compose up -d
+# Stop and remove containers
+docker compose down
+# Start containers without creating
+docker compose start
+# Stop containers without removing them
+docker compose stop
